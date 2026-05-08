07d78ba599df45a1c16a638fc02bc9c77062259c
Amanda Karoline <amandakarolynee5@gmail.com>
2026-05-08 13:03:10 -0300

    Adiciona Open Graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,29 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Clube Clinosp Prime",
+  description: "Programa de fidelidade premium da Clinosp Prime",
+
   icons: {
     icon: "/favicon.ico",
+  },
+
+  openGraph: {
+    title: "Clube Clinosp Prime",
+    description: "Programa de fidelidade premium da Clinosp Prime",
+    url: "https://clube.avaliacaoclinosprime.com.br/clube",
+    siteName: "Clube Clinosp Prime",
+
+    images: [
+      {
+        url: "/preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Clube Clinosp Prime",
+      },
+    ],
+
+    locale: "pt_BR",
+    type: "website",
   },
 };
 
