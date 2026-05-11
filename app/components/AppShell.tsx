3a575f2aeb100a40ed112088e44109907428bba4
Amanda Karoline <amandakarolynee5@gmail.com>
2026-05-11 11:44:47 -0300

    Ajusta responsividade do sidebar

--- app/components/AppShell.tsx
+++ app/components/AppShell.tsx
@@ -20,7 +20,15 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
     <div className="flex">
       <Sidebar />
 
-      <main className="lg:ml-80 w-full min-h-screen">
+      <main
+        className="
+          w-full
+          min-h-screen
+          lg:ml-[240px]
+          xl:ml-[260px]
+          2xl:ml-[280px]
+        "
+      >
         {children}
       </main>
     </div>
